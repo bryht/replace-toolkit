233d6d89644b104355320190c21ba8cd085bca29
Ming Li <ming.li@yunify.nl>
2019-03-16 15:05:19 +0100

    add -f -c chane content

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const path = require('path');
 
 
 var args = process.argv.slice(2);
-if (args.length!=2) {
-    console.log("replace-toolkit oldname newname");
+var isReanmeFile = true;
+var isRenameContent = true;
+if (args.length < 2) {
+    console.log("replace-toolkit oldname newname -f -c");
     return;
+}
+if (args.includes("-f")) {
+    isReanmeFile = true;
+    isRenameContent=false;
+    var index = args.indexOf("-f");
+    args.splice(index, 1);
+}
+if (args.includes("-c")) {
+    isReanmeFile=false;
+    isRenameContent = true;
+    var index = args.indexOf("-c");
+    args.splice(index, 1);
 }
 var oldValue = args[0];
 var newValue = args[1];
@@ -16,7 +30,7 @@ var newValue = args[1];
 renameFilesSync('./');
 
 
-var exts = [".cs", ".json", ".sln", ".csproj", ".dockerignore", ".gitignore", ".Production",".yml"];
+var exts = [".cs", ".json", ".sln", ".csproj", ".dockerignore", ".gitignore", ".Production", ".yml"];
 
 async function renameFilesSync(dir) {
 
@@ -26,7 +40,7 @@ async function renameFilesSync(dir) {
         var fullName = dir + "/" + name;
         if (name.includes(oldValue)) {
 
-             fullName = await replaceFileName(fullName, name);
+            fullName = await replaceFileName(fullName, name);
         }
         var state = await fs.statSync(fullName);
         if (state.isDirectory()) {
@@ -39,19 +53,26 @@ async function renameFilesSync(dir) {
 }
 
 async function replaceFileName(oldFullName, oldName) {
+    if (isReanmeFile==false) {
+        return oldFullName;
+    }
     var newFullName = oldFullName.slice(0, -(oldName.length)) + oldName.replace(oldValue, newValue);
     await fs.renameSync(oldFullName, newFullName);
-    console.log("+++++++"+newFullName);
+    console.log("+++++++" + newFullName);
     return newFullName;
 }
 
 function changeFileContentSync(file) {
+    if (isRenameContent==false) {
+        return;
+    }
+
     fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
         var result = data.replace(new RegExp(oldValue, 'g'), newValue);
-        console.log("======="+file);
+        console.log("=======" + file);
         fs.writeFile(file, result, 'utf8', function (err) {
             if (err) return console.log(err);
         });
